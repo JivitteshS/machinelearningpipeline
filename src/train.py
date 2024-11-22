7a3d109dfda1714da84be56f76f008af1e796265
Jivittesh S <jivittesh@gmail.com>
2024-11-22 09:38:20 +0530

    train.py

--- src/train.py
+++ src/train.py
@@ -19,7 +19,7 @@ os.environ["MLFLOW_TRACKING_PASSWORD"]="2b99e4d1b45979c2beff7cee9a91805fdd76ad4e
 
 def hyperparameter_tuning(X_train, y_train,param_grid):
     rf=RandomForestClassifier()
-    grid_search = GridSearchCV(estimator=rf,param_grid=param_grid,cv=3,n_job=1)
+    grid_search = GridSearchCV(estimator=rf,param_grid=param_grid,cv=3,n_jobs=1)
     grid_search.fit(X_train, y_train)
 
     return grid_search
@@ -59,9 +59,47 @@ def train(data_path,model_path,random_state,n_estimators,max_depth):
         # predict abd evaluate the model
         y_pred = best_model.predict(X_test)
 
-        accuracy_score = accuracy_score(y_test, y_pred)
+        accuracy = accuracy_score(y_test, y_pred)
         
-        print(f"Accuracy score: {accuracy_score}")
+        print(f"Accuracy score: {accuracy}")
+
+
+        ## Log Additional metrics
+
+        mlflow.log_metric("accuracy_score", accuracy)
+        mlflow.log_param("best_n_estimators", grid_search.best_params_["n_estimators"])
+        mlflow.log_param("best_max_depth", grid_search.best_params_["max_depth"])
+        mlflow.log_param("best_min_samples_split", grid_search.best_params_["min_samples_split"])
+        mlflow.log_param("best_min_samples_leaf", grid_search.best_params_["min_samples_leaf"])
+
+        ## Log the confusion matrix and classification report
+
+        cm=confusion_matrix(y_test,y_pred)
+        cr=classification_report(y_test,y_pred)
+
+        mlflow.log_text(str(cm),"confusion matrix.txt")
+        mlflow.log_text(str(cr),"classification report.txt")
+
+        tracking_url_type_store=urlparse(mlflow.get_tracking_uri()).scheme
+
+        if tracking_url_type_store!='file':
+            mlflow.sklearn.log_model(best_model,"model",registered_model_name="Best Model")
+        else:
+            mlflow.sklearn.log_model(best_model,"model",signature=signature)
+
+        ## Create the directory to save the model
+        os.makedirs(os.path.dirname(model_path),exist_ok=True)
+
+        filename=model_path
+        pickle.dump(best_model,open(filename,'wb'))
+
+        print(f"Model saved to {model_path}")
+
+
+if __name__ == '__main__':
+    train(params['data'],params['model'],params['random_state'],params["n_estimators"],
+    params["max_depth"])
+
 
 
 
